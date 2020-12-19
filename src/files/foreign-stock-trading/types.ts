e1af53c3ce162571f83b5bc845d590bcebc43828
sangotaro <sango0715@gmail.com>
2020-12-19 19:06:48 +0900

    feat: ForeignStockTradingData の型を適切にした

--- src/files/foreign-stock-trading/types.ts
+++ src/files/foreign-stock-trading/types.ts
@@ -1,4 +1,4 @@
-// TODO: number や Date を使う
+// TODO: Date を使う
 export type ForeignStockTradingData = {
   国内約定年月日: string;
   現地約定年月日: string;
@@ -11,20 +11,20 @@ export type ForeignStockTradingData = {
   売買: string;
   決済方法: string;
   "自己・委託": string;
-  為替レート: string;
+  為替レート: number;
   市場: string;
   口座区分: string;
-  約定数量: string;
-  約定価格: string;
-  約定金額: string;
-  現地手数料等: string;
-  "現地精算金額(外貨)": string;
-  "現地精算金額(円貨)": string;
-  "国内手数料(外貨)": string;
-  "国内手数料(円貨)": string;
-  "消費税(外貨)": string;
-  "消費税(円貨)": string;
-  "受渡金額(外貨)": string;
-  "受渡金額(円貨)": string;
-  備考: string;
+  約定数量: number;
+  約定価格: number;
+  約定金額: number;
+  現地手数料等: number;
+  "現地精算金額(外貨)": number;
+  "現地精算金額(円貨)": number;
+  "国内手数料(外貨)": number;
+  "国内手数料(円貨)": number;
+  "消費税(外貨)": number;
+  "消費税(円貨)": number;
+  "受渡金額(外貨)": number; // TODO: 円貨決済の場合は null になる?
+  "受渡金額(円貨)": number | null;
+  備考: string | null;
 };
