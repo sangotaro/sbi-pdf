97239ec596e55442dde7c64fb62dc37054ff6b1e
Shotaro Watanabe <shotaro.watanabe0715@gmail.com>
2021-01-09 22:13:02 +0900

    feat: dividend command (#215)

--- src/base.ts
+++ src/base.ts
@@ -1,13 +1,17 @@
 import fs from "fs";
+import os from "os";
 import { isAbsolute, join } from "path";
 import { promisify } from "util";
 
 import { Command } from "@oclif/command";
 import expandTilde from "expand-tilde";
+import pLimit from "p-limit";
+
+import { tabula } from "./tabula";
 
 const readdir = promisify(fs.readdir);
 
-export abstract class Base extends Command {
+export abstract class Base<T> extends Command {
   async findFiles(pathString: string): Promise<string[]> {
     const path = expandTilde(pathString);
     const absolutePath = isAbsolute(path) ? path : join(process.cwd(), path);
@@ -31,5 +35,33 @@ export abstract class Base extends Command {
     this.debug(`${files.length} pdf files found`);
 
     return files;
+  }
+
+  async extractTables(pdfFile: string): Promise<unknown[]> {
+    let result;
+    try {
+      result = await tabula(`-g -l -f JSON -p all ${pdfFile}`);
+    } catch (error) {
+      this.error("tabula failed");
+    }
+    const tables = JSON.parse(result);
+    this.debug(`PDF (${pdfFile}) 内のテーブル数: ${tables.length}`);
+    return tables;
+  }
+
+  abstract extractItems(tables: unknown[]): Promise<T[]>;
+
+  async batchExtract(files: string[]): Promise<T[]> {
+    const limit = pLimit(os.cpus().length - 1);
+    return (
+      await Promise.all(
+        files.map((f) =>
+          limit(async () => {
+            const tables = await this.extractTables(f);
+            return await this.extractItems(tables);
+          })
+        )
+      )
+    ).flat();
   }
 }
